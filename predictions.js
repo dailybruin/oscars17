4ffd0f19d6cf579cdb05b3b825e28a62d36bd92a
Austin Lee <ucaustinlee@g.ucla.edu>
2017-02-19 13:07:25 -0800

    uses correct pictures

--- predictions.js
+++ predictions.js
@@ -1,16 +1,13 @@
 $(document).ready(function(){
-    var pictures = ['predictionsimages/lala1.jpg', 'predictionsimages/emma.jpg', 'predictionsimages/denzel.png', 'predictionsimages/viola.jpg', 'predictionsimages/ali.jpg','predictionsimages/zoo.jpg','predictionsimages/damien.jpg', 'predictionsimages/lala.jpg']
+    var pictures = ['predictionsimages/lala1.jpg', 'predictionsimages/emma.jpg', 'predictionsimages/denzel.jpg', 'predictionsimages/viola.jpg', 'predictionsimages/ali.jpg','predictionsimages/zoo.jpg','predictionsimages/damien.jpg', 'predictionsimages/lala2.jpg']
     var text = ['LA LA LAND', 'EMMA STONE', 'DENZEL WASHINGTON', 'VIOLA DAVIS', 'MAHERSHALA ALI', 'ZOOTOPIA', 'DAMIEN CHAZELLE', 'CITY OF STARS']
-    //var graphs = ['images/chart.png']
+    var graphs = ['images/chart.png', 'images/tri.png']
     var textval;
     $("select").change(function(){
         value = $(this).val();
         textval = value;
          $("#slide2").css("background-image", 'url('+pictures[value]+')');
-         //$("#graph").fadeout(400).graphs('url('+graphs[value]+').fadein(400);
+         $("#graph").css("background-image", 'url('+graphs[value]+')');
          $("#winnername").fadeOut(400).text(text[value]).fadeIn(400);
-        //  if(value == 2) {
-        //      $("#winnername").css("color", "#000 !important");
-        //  }
 }).change();
 })
