6b3a8ad1ba6fe131a5c831132a3724e34b5fc735
Austin Lee <ucaustinlee@g.ucla.edu>
2017-02-20 17:05:26 -0800

    fix spacing and add graphs

--- predictions.js
+++ predictions.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var pictures = ['predictionsimages/lala1.jpg', 'predictionsimages/emma.jpg', 'predictionsimages/denzel.jpg', 'predictionsimages/viola.jpg', 'predictionsimages/ali.jpg','predictionsimages/zoo.jpg','predictionsimages/damien.jpg', 'predictionsimages/lala2.jpg']
     var text = ['LA LA LAND', 'EMMA STONE', 'DENZEL WASHINGTON', 'VIOLA DAVIS', 'MAHERSHALA ALI', 'ZOOTOPIA', 'DAMIEN CHAZELLE', 'CITY OF STARS']
     var creds = ['Photo Credit – Blair Raughley/Invision for Lionsgate/AP Images', 'Photo Credit – Dale Robinette', 'Photo Credit – Dale Robinette', 'Photo Credit – Dale Robinette', 'Photo Credit — David Bornfriend/A24', 'Photo Credit – Paramount Pictures', 'Photo Credit – Paramount Pictures', 'Photo Credit - Walt Disney Pictures']
-    var graphs = ['images/chart.png', 'images/tri.png']
+    var graphs = ['charts/chart1.png', 'charts/chart2.png']
     var textval;
     $("select").change(function(){
         value = $(this).val();
